14eaae2d72476676b32d85fe5a63e361615acbb6
Piyush Kumar Nayak <107127794+piyush932@users.noreply.github.com>
2026-09-18 10:09:45 +0530

    Fix crash: restore real export names (createOrderController/capturePaymentController) in paymentController.js; apply S3 guard directly in messageController.js instead of manual patch

--- Messaging-Slack-Backend-master/src/controllers/messageController.js
+++ Messaging-Slack-Backend-master/src/controllers/messageController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 
-import { s3 } from '../config/awsConfig.js';
+import { s3, isS3Configured } from '../config/awsConfig.js';
 import { AWS_BUCKET_NAME } from '../config/serverConfig.js';
 import { getMessagesService } from '../services/messageService.js';
 import {
@@ -36,6 +36,13 @@ export const getMessages = async (req, res) => {
 };
 
 export const getPresignedUrlFromAWS = async (req, res) => {
+  if (!isS3Configured) {
+    return res
+      .status(StatusCodes.NOT_IMPLEMENTED)
+      .json(internalErrorResponse({
+        message: 'Image upload is disabled in local/demo mode. Set AWS_* env vars to enable.'
+      }));
+  }
   try {
     const url = await s3.getSignedUrlPromise('putObject', {
       Bucket: AWS_BUCKET_NAME,
@@ -45,14 +52,11 @@ export const getPresignedUrlFromAWS = async (req, res) => {
     return res
           .status(StatusCodes.OK)
           .json(successResponse(url, 'Presigned URL generated successfully'));
-          
+
   } catch (err) {
     console.log('Error in getPresignedUrlFromAWS', err);
-    if(err.statusCode) {
-      return res.status(err.statusCode).json(customErrorResponse(err));
-    }
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(internalErrorResponse(err));
   }
-}
+};
