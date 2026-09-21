f67ddad52f47349e2e2e368846e60738d92d11b0
Piyush Kumar Nayak <107127794+piyush932@users.noreply.github.com>
2026-09-21 19:36:56 +0530

    Fix blank /home on expired JWT: add axios response interceptor to clear session and redirect to signin on 401/403

--- Message-Slack-Frontend-master/src/config/axiosConfig.js
+++ Message-Slack-Frontend-master/src/config/axiosConfig.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_API_URL
 });
+
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        const status = error?.response?.status;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            if (window.location.pathname !== '/auth/signin') {
+                window.location.href = '/auth/signin';
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
